2b0b925c44626de92e881d9e1001bd31c9cba164
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-22 23:18:18 +0300

    Changed to Typing

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,9 +3,10 @@
 Module contains function named index_range
  that takes two integer arguments page and page_size.
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Function that takes two integer arguments page and page_size.
 
