1489deac34e9a2a0d1d04e561edef007a2fb2af9
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-25 01:13:18 +0300

    Task 0 Done

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module contains class BasicCache
+"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Basic Caching class
+    """
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
---
+++ 0x01-caching/README.md
@@ -0,0 +1,1 @@
+# Cache concept implementation
