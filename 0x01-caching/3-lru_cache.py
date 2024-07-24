f3d0a6983b3906db12aff3a8170de24d6b4cc656
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-25 02:18:53 +0300

    test

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Module contains class BasicCache
+"""
+from collections import OrderedDict, Counter
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    Basic Caching class implments the lifo concept
+    Least recently used item
+    """
+
+    def __init__(self):
+        """
+        Class Constructor
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache."""
+        if key and item:
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                    lru_key, _ = self.cache_data.popitem(True)
+                    print("DISCARD:", lru_key)
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key, last=False)
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item by key."""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
