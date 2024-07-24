d469ab98134a880bf76fd799940bb243599c5d15
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-25 01:21:41 +0300

    Task 1 done

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module contains class BasicCache
+"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Basic Caching class implments the fifo concept
+    First in firt out concept (Queue)
+    """
+
+    def __init__(self):
+        """
+        Class Constructor
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                discard_key = self.queue.pop(0)
+                del self.cache_data[discard_key]
+                print("DISCARD: {}".format(key))
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
