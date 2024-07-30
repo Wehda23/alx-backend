e4e645d249b1a8d14b3786f3d2c9073ca8e852f2
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-31 01:44:31 +0300

    Task 1 Up for Checkers

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Module to launch the flask application
+Task 1
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from typing import Any
+
+
+class Config:
+    """Configuration class for the Flask application"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Declare flask application
+app: Flask = Flask(__name__, template_folder="./templates")
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+
+@app.route("/")
+def home() -> Any:
+    """
+    Home page route
+
+    Returns:
+        HTML: `0-index.html` in the `./templates` directory
+    """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
