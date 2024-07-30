1462068a77a742b016c0737e8c5f003faaae7ffe
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-31 01:33:20 +0300

    Test for annotations and documentations

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -7,11 +7,17 @@ from typing import Any
 
 
 # Declare flask application
-app = Flask(__name__, template_folder="./templates")
+app: Flask = Flask(__name__, template_folder="./templates")
 
 
 @app.route("/")
 def home() -> Any:
+    """
+    Home page route
+
+    Returns:
+        HTML: `0-index.html` in the `./templates` directory
+    """
     return render_template("0-index.html")
 
 
