811974fc5d68f7071665b1ba4427be44f7adacc7
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-31 02:19:57 +0300

    test

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -3,9 +3,29 @@
 Module to launch the flask application
 Task 2
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, gettext
-from typing import Any
+from typing import Any, Dict, Union
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {
+        "name": "Teletubby",
+        "locale": None,
+        "timezone": "Europe/London",
+    },
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id."""
+    login_id = request.args.get("login_as")
+    if login_id:
+        return users.get(int(login_id))
+    return None
 
 
 class Config:
@@ -22,6 +42,13 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
 babel = Babel(app)
+
+
+@app.before_request
+def before_request() -> None:
+    """Performs some routines before each request's resolution."""
+
+    g.user = get_user()
 
 
 @babel.localeselector
@@ -50,7 +77,7 @@ def home() -> Any:
     home_title = gettext("home_title")
     home_header = gettext("home_header")
     return render_template(
-        "4-index.html", home_title=home_title, home_header=home_header
+        "5-index.html", home_title=home_title, home_header=home_header
     )
 
 
